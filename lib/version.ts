5761b88c95e07d8570d25bb228de3872f25c38ef
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-20 01:52:30 -0700

    Add files via upload

---
+++ lib/version.ts
@@ -0,0 +1,5 @@
+export const DATASET_VERSION = "2026-07-20-multisource-2022plus";
+export const RULES_VERSION = "2.2";
+export const CATEGORY_SET_VERSION = "1.5";
+export const MAX_YEAR_SPREAD = 3;
+export const MINIMUM_OBSERVATION_YEAR = 2022;
