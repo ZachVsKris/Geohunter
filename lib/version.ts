0337da4a6af3b2392f8d10eda3d8670239be5183
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-20 02:23:17 -0700

    Delete lib directory

--- lib/version.ts
+++
@@ -1,5 +0,0 @@
-export const DATASET_VERSION = "2026-07-20-multisource-2022plus-fast2";
-export const RULES_VERSION = "2.3";
-export const CATEGORY_SET_VERSION = "1.5";
-export const MAX_YEAR_SPREAD = 3;
-export const MINIMUM_OBSERVATION_YEAR = 2022;
