2bbad957da1c3147f52c38f6e5d6326e7e760191
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-23 11:53:33 -0700

    Delete app directory

--- app/daily/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Normal Daily | GeoStats" };
-
-export default function DailyPage() {
-  return <GeoSecondComingGame initialDifficulty="normal" />;
-}
