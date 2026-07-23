793f386efa283a73a8e15d2fc0b9fa4b095f3040
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-22 22:55:52 -0700

    Delete lib directory

--- lib/version.ts
+++
@@ -1,4 +0,0 @@
-export const DATASET_VERSION = "2026-07-20-worldbank-76-v9baseline";
-export const RULES_VERSION = "2.5";
-export const CATEGORY_SET_VERSION = "10A";
-export const MAX_YEAR_SPREAD = 3;
