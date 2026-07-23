b3669d4a17a07cb058b8d7bbd9b4a4710b2c0bd8
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-23 10:29:47 -0700

    Delete app directory

--- app/random/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Random Challenge | GeoStats" };
-
-export default function RandomPage() {
-  return <GeoSecondComingGame initialMode="random" initialDifficulty="easy" />;
-}
