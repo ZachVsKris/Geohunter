591d1c4e857d07f1ca1e0141d83666b8802f8868
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-23 09:45:34 -0700

    Delete app directory

--- app/daily/expert/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Expert Daily | GeoStats" };
-
-export default function ExpertDailyPage() {
-  return <GeoSecondComingGame />;
-}
