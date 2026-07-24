f9f62f533d6befd282609e6d7a6161fb883da789
ZachVsKris <114275017+ZachVsKris@users.noreply.github.com>
2026-07-24 00:32:58 -0700

    Delete app directory

--- app/daily/easy/page.tsx
+++
@@ -1,7 +0,0 @@
-import GeoSecondComingGame from "../../../components/GeoSecondComingGame";
-
-export const metadata = { title: "Easy Daily | GeoStats" };
-
-export default function EasyDailyPage() {
-  return <GeoSecondComingGame initialDifficulty="easy" />;
-}
